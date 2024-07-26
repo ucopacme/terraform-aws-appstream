691d19667b692828cc427a8546443e4dd12f7728
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-07-26 16:24:56 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "stack_name" {
   description = "The name of the AppStream 2.0 fleet"
   value       = aws_appstream_fleet.this.name
   }
+
+output "stack_arn" {
+  description = "stack ARN"
+  value       = aws_appstream_stack.this.arn
+  }
