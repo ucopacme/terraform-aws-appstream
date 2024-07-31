20879e83160c80daa62825a678be948881cdd9e0
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-07-31 15:21:02 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,8 +127,8 @@ resource "aws_appstream_fleet_stack_association" "association" {
 
 ###
 resource "aws_appautoscaling_target" "this" {
-  max_capacity       = 10
-  min_capacity       = 1
+  max_capacity       = var.max_capacity
+  min_capacity       = var.min_capacity
   resource_id        = "fleet/${aws_appstream_fleet.this.name}"
   scalable_dimension = "appstream:fleet:DesiredCapacity"
   service_namespace  = "appstream"
@@ -143,7 +143,7 @@ resource "aws_appautoscaling_policy" "scale_up" {
   service_namespace  = aws_appautoscaling_target.this.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 75.0
+    target_value = var.scale_up
     predefined_metric_specification {
       predefined_metric_type = "AppStreamAverageCapacityUtilization"
     }
@@ -161,7 +161,7 @@ resource "aws_appautoscaling_policy" "scale_down" {
   service_namespace  = aws_appautoscaling_target.this.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value = 25.0
+    target_value = var.scale_down
     predefined_metric_specification {
       predefined_metric_type = "AppStreamAverageCapacityUtilization"
     }
