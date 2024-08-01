c189357350d5993f1de457d126591a603448b40c
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-08-01 10:57:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -187,7 +187,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
   threshold           = var.threshold_up
 
   dimensions = {
-    FleetName = aws_appstream_fleet.this.name
+    Fleet = aws_appstream_fleet.this.name
   }
 
   alarm_actions = [aws_appautoscaling_policy.scale_up[0].arn]
